320af6161629a5f6dc6b0c5f6edf24df4cd90e06
Mu31k <66777893+Mu31k@users.noreply.github.com>
2023-06-17 23:26:54 +0500

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,47 @@
+import json
+import re
+from datetime import datetime, timedelta
+import datetime
+from asyncio import Lock
+
+start_lock = Lock()
+
+
+#from aiogram.types import InputMediaPhoto, InputMediaVideo
+
+
+from aiogram import Dispatcher, Bot, types, executor
+import sqlite3
+from aiogram.utils.exceptions import BotBlocked, BadRequest, MessageTextIsEmpty
+
+# from keyboard import adm, iad, kb0, kb1, kb2, kb3, kb4
+# from config import *
+import os
+
+from gspread import Client, Spreadsheet, Worksheet
+
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+# 'proxy_url': PROXY_URL,
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def to_start(msg: types.Message):  # SUBSCRIBE='ads'
+    await msg.answer('Hello')
+
+
+
+@dp.message_handler()
+async def on_startup(_):
+    print('❇Бот запущен!❇')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=on_startup)
+
